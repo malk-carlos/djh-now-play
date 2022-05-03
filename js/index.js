0a8027ee668feeeb5fdaf5ad75f713dbc9816813
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2022-05-03 09:48:01 +0900

    初回コミット

---
+++ js/index.js
@@ -0,0 +1,31 @@
+function customview() {
+    let url = "https://sheets.googleapis.com/v4/spreadsheets/1e6bp_MizsC1qxmnmoIiOCx0rdFiZf8Vf3s1CpRIDIHE/values/get?key=AIzaSyBrH5j4PgNdtfAX4yM5_GUvSjeipA7j72M";
+    return fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(custom) {
+            return custom
+        });
+}
+
+
+function contents() {
+    //スプレッドシート格納配列作成
+
+    //API起動
+    let custom = customview();
+    Promise.resolve(custom).then(function(customs) {
+        document.getElementById('TitleText').innerHTML = "<a href='" + customs.values[0][3] + "'>" + customs.values[0][2] + "</a>";
+        document.getElementById("videoplayer").src = "https://www.youtube.com/embed/" + customs.values[0][1];
+        document.getElementById("thumbnail").src = "https://img.youtube.com/vi/" + customs.values[0][1] + "/maxresdefault.jpg";
+        console.log("https://img.youtube.com/vi/" + customs.values[0][1] + "/maxresdefault.jpg")
+
+
+
+
+    })
+
+}
+
+contents()
