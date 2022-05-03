e0aa489dee2cf1be78ce94f8a5abc7abb14d7055
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2022-05-03 13:25:40 +0900

    コミット

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 function customview() {
     let url = "https://sheets.googleapis.com/v4/spreadsheets/1e6bp_MizsC1qxmnmoIiOCx0rdFiZf8Vf3s1CpRIDIHE/values/get?key=AIzaSyBrH5j4PgNdtfAX4yM5_GUvSjeipA7j72M";
     return fetch(url)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(custom) {
+        .then(function (custom) {
             return custom
         });
 }
@@ -15,17 +15,41 @@ function contents() {
 
     //API起動
     let custom = customview();
-    Promise.resolve(custom).then(function(customs) {
+    Promise.resolve(custom).then(function (customs) {
         document.getElementById('TitleText').innerHTML = "<a href='" + customs.values[0][3] + "'>" + customs.values[0][2] + "</a>";
         document.getElementById("videoplayer").src = "https://www.youtube.com/embed/" + customs.values[0][1];
         document.getElementById("thumbnail").src = "https://img.youtube.com/vi/" + customs.values[0][1] + "/maxresdefault.jpg";
         console.log("https://img.youtube.com/vi/" + customs.values[0][1] + "/maxresdefault.jpg")
 
 
-
-
     })
+
 
 }
 
+function color() {
+    const colorThief = new ColorThief();
+    const img1 = document.querySelector('img#thumbnail');
+    var color;
+    // Make sure image is finished loading
+    if (img1.complete) {
+        color = colorThief.getColor(img1);
+    } else {
+        image.addEventListener('load', function () {
+            color = colorThief.getColor(img1);
+        });
+    }
+    var rbg = color[0] + "," + color[1] + "," + color[2];
+    console.log
+    document.getElementById("label1").style.color = "rgb(" + rbg + ")";//機関車の画像の色
+    console.log(color)
+}
+
 contents()
+setTimeout(() => {
+    color()
+}, 2000);
+
+
+
+
